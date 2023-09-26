d613a4f9d8932ab429ef9a0d846841a52dc68c61
#FF0050 <135780151+Joa0Gustav0@users.noreply.github.com>
2023-09-26 11:22:48 -0300

    📚 Studying React | Events

--- ex002/src/components/SubmitButton.jsx
+++ ex002/src/components/SubmitButton.jsx
@@ -1,8 +1,13 @@
 import styles from './SubmitButton.module.css'
 
+
 function SubmitButton(){
+    function logMessage(){
+        console.log("Confirmado!")
+    }
     return (
-        <input type="button" className={styles.confirmButton} id="confirm-button" value="Confirmar"/>
+        <input type="button" className={styles.confirmButton} id="confirm-button" value="Confirmar"
+        onClick={logMessage}/>
     )
 }
 
