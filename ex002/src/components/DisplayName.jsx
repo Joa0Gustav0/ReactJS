11b3187ddf8ff4a7b44b7b3de5ec13f5540b7329
#FF0050 <135780151+Joa0Gustav0@users.noreply.github.com>
2023-09-26 09:47:57 -0300

    📚 Studying React | defaultProps

--- ex002/src/components/DisplayName.jsx
+++ ex002/src/components/DisplayName.jsx
@@ -7,11 +7,16 @@ export default function DisplayName({name ,currentJob, workingTime}){
         currentJob: propTypes.string,
         workingTime: propTypes.number
     }
+    DisplayName.defaultProps = {
+        name: "visitante",
+        currentJob: "trabalhador",
+        workingTime: "alguns"
+    }
     return(
         <div>
-            <h1 className={styles.welcomeTitle}>Olá, <span>{name || "Visitante"}</span>!</h1>
+            <h1 className={styles.welcomeTitle}>Olá, <span>{name}</span>!</h1>
             <h2 className={styles.jobText}>Você é <span>{currentJob}</span>.</h2>
-            <h2 className={styles.jobText}>Trabalhando há <span>{workingTime ?? "alguns "} anos</span>.</h2>
+            <h2 className={styles.jobText}>Trabalhando há <span>{workingTime.toFixed(1)}  anos</span>.</h2>
         </div>
     )
 }
