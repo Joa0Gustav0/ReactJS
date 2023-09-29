34f1d799d1ba969b172b41abb3968538c37aa89f
#FF0050 <135780151+Joa0Gustav0@users.noreply.github.com>
2023-09-28 22:51:32 -0300

    📚 Studying React

--- ex002/src/components/UsersList.js
+++ ex002/src/components/UsersList.js
@@ -1,13 +1,29 @@
+import styles from './styles/UsersList.module.css'
+
 export default function UserList({usersArray}){
+    console.log(usersArray)
     return(
         <>
-            <h1>Usuários Cadastrados:</h1>
-            {
-                    usersArray.map((currentUser) => (
-                        <p>Name: {currentUser.name} | Job: {currentUser.job}</p>
-                    ))
-            }
-            {console.log(usersArray)}
+            <h1 className={styles.title}>Usuários Cadastrados:</h1>
+            <table className={styles.usersBox}>
+                <thead>
+                    <tr>
+                        <th className={styles.usersBox__title} key="name-title">Nome</th>
+                        <th className={styles.usersBox__title}>Profissão</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {usersArray.length > 0 && (
+                        usersArray.map((currentUser, currentI) => (
+
+                            <tr key={currentUser.name + currentI}>
+                                <td className={styles.usersBox__data}>{currentUser.name}</td>
+                                <td className={styles.usersBox__data}>{currentUser.job}</td>
+                            </tr>
+                        )))
+                    }
+                </tbody>
+            </table>
         </>
     )
 }
